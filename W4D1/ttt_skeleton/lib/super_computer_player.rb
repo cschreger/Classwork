f147d268a5359c5a9bc8cfea07c942a8b165e0bf
Cade Schreger <cschreger@gmail.com>
2020-06-01 17:37:39 -0400

    Super Computer Start

--- W4D1/ttt_skeleton/lib/super_computer_player.rb
+++ W4D1/ttt_skeleton/lib/super_computer_player.rb
@@ -2,8 +2,24 @@ require_relative 'tic_tac_toe_node'
 
 class SuperComputerPlayer < ComputerPlayer
   def move(game, mark)
+    node = TicTacToeNode.new(Board.new,mark)
+    moves = []
+
+    children = node.children
+    children.each do |child|
+      return child.prev_move_pos if child.winning_node?(mark)
+      moves << child.prev_move_pos unless child.losing_node?(mark)
+    end
+
+    if moves.empty?
+      raise "loserrr" 
+    else
+      return moves
+    end
   end
+
 end
+
 
 if __FILE__ == $PROGRAM_NAME
   puts "Play the brilliant computer!"
