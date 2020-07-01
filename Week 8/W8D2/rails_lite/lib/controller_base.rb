119803cfa8ef5788844e7db380fb3d00eafebc40
Cade Schreger <cschreger@gmail.com>
2020-07-01 13:38:54 -0400

    Controller base

--- Week 8/W8D2/rails_lite/lib/controller_base.rb
+++ Week 8/W8D2/rails_lite/lib/controller_base.rb
@@ -7,21 +7,36 @@ class ControllerBase
   attr_reader :req, :res, :params
 
   # Setup the controller
-  def initialize(req, res)
+  def initialize(req, res, route_params = {})
+    @req = req
+    @res = res
+    @params = route_params.merge(req.params)
+    @protect_from_forgery ||= false
   end
 
   # Helper method to alias @already_built_response
   def already_built_response?
+    @already_built_response == @res
   end
 
   # Set the response status code and header
   def redirect_to(url)
+    raise "Cannot redirect twice" if already_built_response?
+    @res.location = url
+    @res.status = "302"
+
+    @already_built_response = @res
   end
 
   # Populate the response with content.
   # Set the response's content type to the given type.
   # Raise an error if the developer tries to double render.
   def render_content(content, content_type)
+    raise "Cannor render content twice" if already_built_response?
+    @res.write(content)
+    @res['Content Type'] = content_type
+
+    @already_built_response = @res
   end
 
   # use ERB and binding to evaluate templates
@@ -31,6 +46,7 @@ class ControllerBase
 
   # method exposing a `Session` object
   def session
+    @session ||= Session.new(@req)
   end
 
   # use this with the router to call action_name (:index, :show, :create...)
